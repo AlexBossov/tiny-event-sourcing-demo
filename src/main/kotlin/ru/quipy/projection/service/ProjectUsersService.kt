2a2dc34f4f880d7aec95568ae7fe50df93cc78e7
AnnemariaRe <annemariarepenko@gmail.com>
2023-11-15 19:05:27 +0300

    feat: add projectUsers subscribers

--- src/main/kotlin/ru/quipy/projection/service/ProjectUsersService.kt
+++ src/main/kotlin/ru/quipy/projection/service/ProjectUsersService.kt
@@ -3,12 +3,16 @@ package ru.quipy.projection.service
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import ru.quipy.api.ProjectCreatedEvent
+import ru.quipy.api.UserAddedToProjectEvent
 import ru.quipy.api.UserAggregate
 import ru.quipy.projection.UserEventsSubscriber
 import ru.quipy.projection.dto.UserDto
 import ru.quipy.projection.repository.ProjectUsersRepository
 import ru.quipy.projection.repository.UserRepository
+import ru.quipy.projection.view.ProjectUsersViewDomain
 import ru.quipy.streams.annotation.AggregateSubscriber
+import ru.quipy.streams.annotation.SubscribeEvent
 import java.util.*
 
 @Service
@@ -18,6 +22,30 @@ class ProjectUsersService(
     val projectUsersRepository: ProjectUsersRepository
 ) {
     val logger: Logger = LoggerFactory.getLogger(UserEventsSubscriber::class.java)
+
+    @SubscribeEvent
+    fun projectCreatedSubscriber(event: ProjectCreatedEvent) {
+        logger.info("Project created: {}", event.title)
+        projectUsersRepository.save(
+            ProjectUsersViewDomain.ProjectUsersView(
+                UUID.randomUUID(),
+                event.projectId,
+                event.userId
+            )
+        )
+    }
+
+    @SubscribeEvent
+    fun userAddedToProjectSubscriber(event: UserAddedToProjectEvent) {
+        logger.info("User added to project: {}", event.name)
+        projectUsersRepository.save(
+            ProjectUsersViewDomain.ProjectUsersView(
+                UUID.randomUUID(),
+                event.projectId,
+                event.userId
+            )
+        )
+    }
 
     fun findUsersByProjectId(projectId: UUID): List<UserDto> {
         val projectUsers = projectUsersRepository.findByProjectId(projectId)
